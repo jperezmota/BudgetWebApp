fc6a440eff21dacebaf84305a7e9455e49fbc606
Jonathan Perez <jonathanperezmota@gmail.com>
2018-07-23 18:04:29 -0400

    Adding main Controllers Modules though IIFE and .tern-project for Javascript auto-complete in ATOM.

---
+++ app.js
@@ -0,0 +1,15 @@
+var budgetController = (function(){
+
+
+})();
+
+var UIController = (function(){
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl){
+
+
+})(budgetController, UIController);
