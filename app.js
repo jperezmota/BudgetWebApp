7b73103917a03c48691042e74d8ecbf61c2ec7f5
Jonathan Perez <jonathanperezmota@gmail.com>
2018-07-26 20:03:12 -0400

    Formatting currency.

--- app.js
+++ app.js
@@ -138,8 +138,25 @@ var UIController = (function(){
     expensesPercentageLabel: '.item__percentage'
   };
 
-  return {
+  var formatNumber = function(num, type){
+    var numSplit, int, dec;
 
+    num = Math.abs(num);
+    num = num.toFixed(2);
+
+    numSplit =  num.split('.');
+    int = numSplit[0];
+
+    if(int.length > 3){
+      int = int.substr(0, int.length - 3) + ', ' + int.substr(int.length - 3, 3);
+    }
+
+    dec = numSplit[1];
+
+    return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec
+  }
+
+  return {
     getInput: function(){
       return {
         type: document.querySelector(DOMstrings.inputType).value, // Will be either Inc or Exp
@@ -155,7 +172,7 @@ var UIController = (function(){
         html = `<div class="item clearfix" id="inc-${obj.id}">
             <div class="item__description">${obj.description}</div>
             <div class="right clearfix">
-                <div class="item__value">+ ${obj.value}</div>
+                <div class="item__value">${formatNumber(obj.value, type)}</div>
                 <div class="item__delete">
                     <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
                 </div>
@@ -166,7 +183,7 @@ var UIController = (function(){
         html = `<div class="item clearfix" id="exp-${obj.id}">
             <div class="item__description">${obj.description}</div>
             <div class="right clearfix">
-                <div class="item__value">- ${obj.value}</div>
+                <div class="item__value">${formatNumber(obj.value, type)}</div>
                 <div class="item__percentage">21%</div>
                 <div class="item__delete">
                     <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button>
@@ -191,9 +208,12 @@ var UIController = (function(){
       fieldsArr[0].focus();
     },
     displayBudget: function(obj){
-      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalIncome;
-      document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExpenses;
+      var type;
+      obj.budget > 0 ? type = 'inc' : type = 'exp';
+
+      document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget, type);
+      document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalIncome, 'inc');
+      document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExpenses, 'exp');
 
 
       if(obj.percentage > 0){
